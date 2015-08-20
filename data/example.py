8f627de96cbdc1497345c0d5258913b263e8e048
freeman <the.freeman.lab@gmail.com>
2015-08-19 21:00:27 -0400

    Add example

--- data/example.py
+++ data/example.py
@@ -1,11 +1,11 @@
 from lightning import Lightning
-from numpy import random, ceil, array
+from numpy import random
 
 lgn = Lightning()
 
 x = random.randn(100) * 15
 y = random.randn(100) * 15
-group = ceil(random.rand(100) * 5)
+group = (random.rand(100) * 5).astype('int')
 size = random.rand(100) * 20 + 5
 
-lgn.scatter(x, y, group=group, size=size, alpha=alpha)
+lgn.scatter(x, y, group=group, size=size)
